92a88bdea969f1b91c654470659acd382181f056
SUNG IL LEE <sungil.lee90@gmail.com>
2020-03-16 22:07:34 -0500

    Corrected Warmup Exercise for 03/16/2020.

---
+++ js/external.js
@@ -0,0 +1,7 @@
+console.log("Hello from external JavaScript.");
+
+alert("Welcome to my Website!");
+
+var userInput = prompt ('What is your favorite color?');
+console.log('The user entered: ' + userInput);
+alert("Great, " + userInput + "  is my favorite color too!");
