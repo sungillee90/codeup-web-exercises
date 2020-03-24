63b862f0e38d91b88c5a937f1365608c52857f90
SUNG IL LEE <sungil.lee90@gmail.com>
2020-03-24 16:54:10 -0500

    Partially finished exercise.

---
+++ js/for_loops.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function showMultiplicationTable (aNumber) {
+    for (var i = 1, j = 1; i < 10, j < 10; i++) {
+        i * j;
+        console.log('Multiplication Table of '+ showMultiplicationTable(2) );
+    }
+}
--- js/while.js
+++ js/while.js
@@ -26,3 +26,26 @@ while ( i <= 65536 ) {
     i = i * 2;
 }
 
+// Problem 2
+
+
+// This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+var numberOfConesToPurchase = Math.floor(Math.random() * 5) + 1;
+
+console.log('Amount of cones to sell today: ' + allCones);
+console.log('I want to purchase ' + numberOfConesToPurchase + ' cones today! :)');
+
+// do-while
+
+do {
+    // var itemsLeft = (allCones - numberOfConesToPurchase)
+    console.log('Items left: ' + allCones);
+    allCones = allCones - numberOfConesToPurchase;
+    if (numberOfConesToPurchase > allCones) {
+        console.log('Cannot sell you ' + numberOfConesToPurchase + ' cones I only have ' + allCones);
+    } else if (allCones === 0) {
+        console.log('Yay I sold them all!');
+    }
+} while (allCones > 0);
