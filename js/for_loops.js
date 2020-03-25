e0d963263fb74adaf7776fea16c6531baa1ffa4f
SUNG IL LEE <sungil.lee90@gmail.com>
2020-03-25 11:41:50 -0500

    Partially Complete Loops Exercise.

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,8 +1,25 @@
 "use strict";
 
 function showMultiplicationTable (aNumber) {
-    for (var i = 1, j = 1; i < 10, j < 10; i++) {
-        i * j;
-        console.log('Multiplication Table of '+ showMultiplicationTable(2) );
+    for (var i = 1; i <= 10; i++) {
+        // for (var j = 0; j <= 10; j++) {
+            var answer = aNumber * i;
+
+            console.log(aNumber + ' x ' + i + ' = ' + answer);
+        //     if(i == 0 && j > 0){
+        //         result += '[' + j + ']';
+        //     }
+        //     else if(j == 0 && i>0){
+        //         result += '[' + i + '] ';
+        //     }
+        //     else if(i>0 && j>0){
+        //         result += (i*j) + ' ';
+        //     }
+        // }
+        // result += '\n'
     }
+    // return "All done!";
 }
+    console.log(showMultiplicationTable(7));
+
+// console.log('Multiplication Table of '+ showMultiplicationTable(7) );
--- js/while.js
+++ js/while.js
@@ -1,7 +1,7 @@
 "use strict";
 console.log("hello from while JS");
 
-// Create a while loop that uses console.log() to create the output shown below
+// Problem 1. Create a while loop that uses console.log() to create the output shown below
 // 2
 // 4
 // 8
@@ -18,17 +18,15 @@ console.log("hello from while JS");
 // 16384
 // 32768
 // 65536
-
+// Multiplying output by 2
 var i = 2;
 
 while ( i <= 65536 ) {
     console.log('Multiple of 2 from 2 to 65536 : '+i);
-    i = i * 2;
+    i = i * 2; // or i *= 2;
 }
 
 // Problem 2
-
-
 // This is how you get a random number between 50 and 100
 var allCones = Math.floor(Math.random() * 50) + 50;
 // This expression will generate a random number between 1 and 5
@@ -40,12 +38,20 @@ console.log('I want to purchase ' + numberOfConesToPurchase + ' cones today! :)'
 // do-while
 
 do {
-    // var itemsLeft = (allCones - numberOfConesToPurchase)
-    console.log('Items left: ' + allCones);
-    allCones = allCones - numberOfConesToPurchase;
-    if (numberOfConesToPurchase > allCones) {
-        console.log('Cannot sell you ' + numberOfConesToPurchase + ' cones I only have ' + allCones);
-    } else if (allCones === 0) {
-        console.log('Yay I sold them all!');
+    var conesToPurchase = Math.floor(Math.random() * 5) + 1;
+    console.log('All cones: ' + allCones + ' Cones to purchase ' + conesToPurchase);
+    // allCones = allCones - conesToPurchase;
+    if (conesToPurchase <= allCones) {
+        //     // I can sell the cones.
+        console.log(conesToPurchase + "Cones sold...");
+        //     // conesToPurchase = conesToPurchase - allCones;
+        allCones -= conesToPurchase;
+    } else {
+        //I do NOT have enough cones to sell.
+        console.log('Cannot sell you ' + conesToPurchase + " cones I only have " + allCones + "...");
     }
+    // allCones -= 1;
+    console.log(allCones);
 } while (allCones > 0);
+
+console.log("Yay! I sold them all!");
