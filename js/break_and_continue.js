7393fca3f24f2cb39845d7d61e56490c9b1ad786
SUNG IL LEE <sungil.lee90@gmail.com>
2020-03-25 14:02:48 -0500

    Finished Exercise.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,5 +1,7 @@
+var userNumber = 0; // To use it later  after while loop
+
 do {
-    var userNumber = Number(prompt('Please enter a number between 1 and 50.'));
+    userNumber = Number(prompt('Please enter a number between 1 and 50.'));
     if (userNumber < 1 || userNumber > 50 ){
         alert(userNumber + " is not between 1 and 50. Please try again.");
     } else if (userNumber % 2 === 0) {
@@ -15,3 +17,12 @@ do {
     // break;
 } while (true);
 
+console.log('Number to skip is: ' + userNumber)
+
+for (var i = 1; i <= 50; i += 2) { // i+= 2 to get the ODD Number
+    if (i === userNumber) {
+        console.log("Yikes!: Skipping number: " + userNumber)
+        continue;
+    }
+    console.log('Here is an ODD number: ' + i);
+}
