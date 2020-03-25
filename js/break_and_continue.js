d4392f6bd1e937b973b317f87fbb9e432b63be31
SUNG IL LEE <sungil.lee90@gmail.com>
2020-03-25 13:59:40 -0500

    Finished Break_and_Continue Exercise.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,6 +1,17 @@
 do {
     var userNumber = Number(prompt('Please enter a number between 1 and 50.'));
-    console.log(userNumber);
-    console.log('Breaking out');
-    break;
+    if (userNumber < 1 || userNumber > 50 ){
+        alert(userNumber + " is not between 1 and 50. Please try again.");
+    } else if (userNumber % 2 === 0) {
+        alert(userNumber + ' is not odd. Please try again.');
+    } else if (isNaN(userNumber)) {
+        alert(userNumber+ ' is not a number. Please try again.');
+    } else {
+        alert('Yay! You entered a valid number.');
+        break;
+    }
+    // console.log(userNumber);
+    // console.log('Breaking out');
+    // break;
 } while (true);
+
