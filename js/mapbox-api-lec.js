1bfa7cfa33993d1c434d2ab982341c27c60822e4
SUNG IL LEE <sungil.lee90@gmail.com>
2020-04-02 13:52:12 -0500

    feat: Use Geocoding to get the coordinates for Codeup.

--- js/mapbox-api-lec.js
+++ js/mapbox-api-lec.js
@@ -20,17 +20,14 @@ console.log('token: ' + mapboxToken);
 // mapboxgl.accessToken = 'pg.adijasldijaldnunifnwle';
 mapboxgl.accessToken = mapboxToken;
 
-var map = new mapboxgl.Map({
-    // https://docs.mapbox.com/mapbox-gl-js/api/
-
-    container: 'map',
-    style: 'mapbox://styles/mapbox/satellite-v9',
-    center: [-96.8057, 32.7787],
-    zoom: 10,
-    logoPosition: "top-right"
-
-
-});
+// var map = new mapboxgl.Map({
+//     // https://docs.mapbox.com/mapbox-gl-js/api/
+//     container: 'map',
+//     style: 'mapbox://styles/mapbox/satellite-v9',
+//     center: [-96.8057, 32.7787],
+//     zoom: 10,
+//     logoPosition: "top-right"
+// });
 
 
 
@@ -48,24 +45,24 @@ var map = new mapboxgl.Map({
 
 // TODO TOGETHER: Add a marker to the map using the following coordinates [-96.8084, 32.7799]. This marker will mark the Sixth Floor Muesume on our map.
 // TODO TOGETHER: Change the color of the marker
-var markerOptions = {
-    color: "teal",
-    draggable: true
-};
-var markerOptionsToM = {
-    color: "blue",
-    draggable: true
-};
-var marker = new mapboxgl.Marker(markerOptions)
-    //Use the .setLngLat() and .addTo() methods to add marker to the map
-    .setLngLat([-96.8057, 32.7787])
-    .addTo(map)
-
-
-var markerToMuseum = new mapboxgl.Marker(markerOptionsToM)
-    //Use the .setLngLat() and .addTo() methods to add marker to the map
-    .setLngLat([-96.8084, 32.7799])
-    .addTo(map)
+// var markerOptions = {
+//     color: "teal",
+//     draggable: true
+// };
+// var markerOptionsToM = {
+//     color: "blue",
+//     draggable: true
+// };
+// var marker = new mapboxgl.Marker(markerOptions)
+//     //Use the .setLngLat() and .addTo() methods to add marker to the map
+//     .setLngLat([-96.8057, 32.7787])
+//     .addTo(map)
+//
+//
+// var markerToMuseum = new mapboxgl.Marker(markerOptionsToM)
+//     //Use the .setLngLat() and .addTo() methods to add marker to the map
+//     .setLngLat([-96.8084, 32.7799])
+//     .addTo(map)
 
 // TODO: Experiment with the color, and setting the LngLat
 // TODO: Update the marker object to make the marker draggable. *Hint: reference the docs!
@@ -82,19 +79,18 @@ var markerToMuseum = new mapboxgl.Marker(markerOptionsToM)
 //          to pass an object put {}
 // var popup = new mapboxgl.Popup({});
 
-var popup = new mapboxgl.Popup()
-    .setHTML("<h1>Codeup Rocks!</h1>")
-    .addTo(map);
+// var popup = new mapboxgl.Popup()
+//     .setHTML("<h1>Codeup Rocks!</h1>")
+//     .addTo(map);
+//
+// marker.setPopup(popup);
+// // TODO TOGETHER: Comment out the popup we just added. Add a popup to the Sixth Floor Musume marker.
+// var popupMuseum = new mapboxgl.Popup()
+//     .setHTML("<h1>Sixth Floor Museum!</h1>")
+//     .addTo(map);
+//
+// markerToMuseum.setPopup(popupMuseum);
 
-marker.setPopup(popup);
-
-var popupMuseum = new mapboxgl.Popup()
-    .setHTML("<h1>Sixth Floor Museum!</h1>")
-    .addTo(map);
-
-markerToMuseum.setPopup(popupMuseum);
-
-// TODO TOGETHER: Comment out the popup we just added. Add a popup to the Sixth Floor Musume marker.
 
 
 // TODO: Review the popup docs. What are some additional options we can pass to the popup?
@@ -110,7 +106,17 @@ markerToMuseum.setPopup(popupMuseum);
 
 // TODO TOGETHER: Using the Geocoder helper function, log the coordinates of Codeup and recenter the map to focus on Codeup. Comment out previous map code.
 
-
+geocode("701 Commerce St. Dallas, Tx. 75202", mapboxToken).then(function(result) {
+   console.log('lng , lat: ', result);
+    var map = new mapboxgl.Map({
+    // https://docs.mapbox.com/mapbox-gl-js/api/
+    container: 'map',
+    style: 'mapbox://styles/mapbox/satellite-v9',
+    center: result,
+    zoom: 16,
+    logoPosition: "top-right"
+});
+});
 
 
 
