d6491c55256422894755aaf66a099dcf47432458
SUNG IL LEE <sungil.lee90@gmail.com>
2021-01-16 19:55:46 -0600

    Day2 of JS

--- js/practiceDay2.js
+++ js/practiceDay2.js
@@ -1,4 +1,47 @@
-// document.write("<h1>This is h1 JS!</h1>helloWorld from JS");
-// var x = prompt("Enter your name: ");
-// alert("Happy New Year! " + x + ", hope you are doing fantastic!");
+'use strict';
 
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the vowelsAndConsonants function.
+ * Print your output using 'console.log()'.
+ */
+function vowelsAndConsonants(s) {
+    let vowels = ['a', 'e', 'i','o', 'u'];
+
+    for (let i = 0; i < s.length; i++) {
+        if(s.charAt(i).match(/[aeiou]/)) {
+            console.log(s.charAt(i));
+        }
+    }
+    for (let i = 0; i < s.length; i++) {
+        if(s.charAt(i).match(/[^aeiou]/)) {
+            console.log(s.charAt(i));
+        }
+    }
+
+}
+
+
+function main() {
+    const s = readLine();
+
+    vowelsAndConsonants(s);
+}
