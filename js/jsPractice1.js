473635f8d8c83b09605722415090ab9540af5d9a
SUNG IL LEE <sungil.lee90@gmail.com>
2021-01-19 16:26:44 -0600

    added getter and setter in js

--- js/jsPractice1.js
+++ js/jsPractice1.js
@@ -49,20 +49,32 @@
 function Circle(radius) {
 
     this.radius = radius;
-
+    // same as private key word in java
     let defaultLocation = { x: 0, y: 0 };
 
-    let computeOptimumLocation = function(factor) {
-
+    this.getDefaultLocation = function () {
+      return defaultLocation;
     }
 
     this.draw = function() {
-        computeOptimumLocation(0.1);
-
         console.log('draw');
     }
+
+    // getter and setter
+    Object.defineProperty(this, 'defaultLocation', {
+        get: function() {
+           return defaultLocation;
+        },
+        set: function(val) {
+            if (!val.x || !val.y) {
+                throw new Error('Invalid Location!');
+            }
+               defaultLocation = val;
+        }
+    });
 }
 
 const circle = new Circle(10);
 circle.draw();
+
 
