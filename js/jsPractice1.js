fe6ca16465af40aa892755c7da31942a44ca05c3
SUNG IL LEE <sungil.lee90@gmail.com>
2021-01-19 14:59:10 -0600

    added encapsulation

--- js/jsPractice1.js
+++ js/jsPractice1.js
@@ -17,28 +17,52 @@
 //
 // employee.getWage();
 
-console.log('hello world from JS practice 1');
 
+// Before OOP
 
-// Factory Function
-function createCircle(radius) {
-   return {
-        radius: radius,
-        draw: function() {
-            console.log('draw');
-        }
-    };
-}
+// console.log('hello world from JS practice 1');
+//
+//
+// // Factory Function
+// function createCircle(radius) {
+//    return {
+//         radius: radius,
+//         draw: function() {
+//             console.log('draw from factory func');
+//         }
+//     };
+// }
+//
+// const circle1 = createCircle(1);
+// circle1.draw();
+//
+// // Constructor Function
+// function Circle(radius) {
+//     this.radius = radius;
+//     this.draw = function () {
+//         console.log('draw from constructor func');
+//     }
+// }
+// const another = new Circle(1);
+// another.draw();
 
-const circle1 = createCircle(1);
-const circle2 = createCircle(6);
-
-// Constructor Function
 function Circle(radius) {
+
     this.radius = radius;
-    this.draw = function () {
+
+    let defaultLocation = { x: 0, y: 0 };
+
+    let computeOptimumLocation = function(factor) {
+
+    }
+
+    this.draw = function() {
+        computeOptimumLocation(0.1);
+
         console.log('draw');
     }
 }
-const another = new Circle(1);
-another.draw();
+
+const circle = new Circle(10);
+circle.draw();
+
