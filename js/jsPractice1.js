5348e2205d2cbc1cf09dd8ac4c8d27493533a373
SUNG IL LEE <sungil.lee90@gmail.com>
2021-01-19 09:55:47 -0600

    Added 2 ways to create circle, factory func, and constructor func

--- js/jsPractice1.js
+++ js/jsPractice1.js
@@ -19,15 +19,26 @@
 
 console.log('hello world from JS practice 1');
 
-const circle = {
-  radius: 1,
-  location: {
-      x: 1,
-      y: 1
-  },
-  draw: function() {
-      console.log('draw');
-  }
-};
 
-circle.draw()
+// Factory Function
+function createCircle(radius) {
+   return {
+        radius: radius,
+        draw: function() {
+            console.log('draw');
+        }
+    };
+}
+
+const circle1 = createCircle(1);
+const circle2 = createCircle(6);
+
+// Constructor Function
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function () {
+        console.log('draw');
+    }
+}
+const another = new Circle(1);
+another.draw();
