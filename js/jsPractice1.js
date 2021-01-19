39d5c7eb0008e84ca42d8725413868d5cb8f665d
SUNG IL LEE <sungil.lee90@gmail.com>
2021-01-19 09:47:42 -0600

    added obj to the js practice

--- js/jsPractice1.js
+++ js/jsPractice1.js
@@ -1,18 +1,33 @@
-let baseSalary = 50000;
-let overTime = 10;
-let rate = 20;
+// let baseSalary = 50000;
+// let overTime = 10;
+// let rate = 20;
+//
+// function getWage(baseSalary, overTime, rate) {
+//     return baseSalary + (overTime * rate);
+// }
+//
+// let employee = {
+//     baseSalary: 30000,
+//     overTime: 10,
+//     rate: 20,
+//     getWage: function () {
+//         return this.baseSalary + (this.overTime * this.rate);
+//     }
+// }
+//
+// employee.getWage();
 
-function getWage(baseSalary, overTime, rate) {
-    return baseSalary + (overTime * rate);
-}
+console.log('hello world from JS practice 1');
 
-let employee = {
-    baseSalary: 30000,
-    overTime: 10,
-    rate: 20,
-    getWage: function () {
-        return this.baseSalary + (this.overTime * this.rate);
-    }
-}
+const circle = {
+  radius: 1,
+  location: {
+      x: 1,
+      y: 1
+  },
+  draw: function() {
+      console.log('draw');
+  }
+};
 
-employee.getWage();
+circle.draw()
